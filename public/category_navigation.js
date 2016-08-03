d9fadd50fdd7f3c93c22761d94b973938f44536f
Shuaibird <shuaibird@icloud.com>
2016-08-03 16:35:22 +0800

    done

--- public/category_navigation.js
+++ public/category_navigation.js
@@ -1,35 +1,122 @@
 +function($) {
+
+  var dataModel = [
+    {
+      heading: '平台概述',
+      section: [
+        {
+          title: '概述',
+          url: '#'
+        },
+        {
+          title: '概述',
+          url: '#'
+        }
+      ]
+    },
+
+    {
+      heading: '快速入门',
+      section: [
+        {
+          title: '概述',
+          url: '#'
+        },
+        {
+          title: '云端管理',
+          url: '#'
+        },
+        {
+          title: '设备端开发',
+          url: '#'
+        }
+      ]
+    },
+
+    {
+      heading: '开发文档',
+      section: [
+        {
+          title: '概述',
+          url: '#'
+        },
+        {
+          title: 'WiFi产品APP开发开源框架使用指南',
+          url: '#'
+        },
+        {
+          title: 'WiFi产品iOS SDK 2.0 集成指南',
+          url: '#'
+        }
+      ]
+    }
+  ]
+
+
+
+  var template = '<div class="category-navigation"><ul>'
+
+  dataModel.forEach(function(data) {
+    var heading = '<div class="category-navigation-heading"><span class="ui-element-triangle">&#9656;</span>' + data.heading +'</div>'
+    template += '<li>' + heading + '<ul class="category-navigation-section">'
+
+    data.section.forEach(function(item) {
+      var menu = '<li><a href="' + item.url + '" title="' + item.title + '">' + item.title + '</a></li>'
+      template += menu
+    })
+
+    template += '</ul></li>'
+  })
+
+  template += '</ul></div>'
+
+
   $(function() {
-    var template = (function () {/*
+    $('.content-sidebar').append($(template))
 
-      <div class="category-navigation">
-        <ul class="category-navigation-level1">
-          <li>
-            <div class="category-navigation-heading">平台概述</div>
-            <ul class="category-navigation-level2">
-              <li>这是板块A</li>
-              <li>这是板块B</li>
-            </ul>
-          </li>
-          <li>
-            <div class="category-navigation-heading">快速入门</div>
-            <ul class="category-navigation-level2">
-              <li>这是板块A</li>
-              <li>这是板块B</li>
-            </ul>
-          </li>
-          <li>
-            <div class="category-navigation-heading">开发文档</div>
-            <ul class="category-navigation-level2">
-              <li>这是板块A</li>
-              <li>这是板块B</li>
-            </ul>
-          </li>
-        </ul>
-      </div>
+    $('.category-navigation-heading').click(function() {
+      var $this = $(this)
+      var categorySection = $this.next()
+      var triangle = $this.find('.ui-element-triangle')
+      categorySection.is(':visible') ? triangle.html('&#9656;') : triangle.html('&#9662;')
+      categorySection.slideToggle()
+    })
 
-    */}).toString().split('\n').slice(1, -1).join('\n')
+    var heading = $('title').text().split('-')[0].trim()
 
-    $('.content-sidebar').append(template)
+    dataModel.forEach(function(item, index) {
+      if(item.heading === heading) {
+        $('.category-navigation-heading').eq(index)
+          .addClass('active')
+          .find('.ui-element-triangle').html('&#9662;') 
+
+        $('.category-navigation-section').eq(index).attr('style', 'display: block')
+      }
+    })
+
   })
 }(jQuery)
+
+
+
+
+// template for reference.
+
+// <div class="category-navigation">
+//   <ul>
+//     <li>
+//       <div class="category-navigation-heading">平台概述</div>
+//       <ul class="category-navigation-section">
+//         <li><a href="#">概述</a></li>
+//         <li><a href="#">GoKit</a></li>
+//       </ul>
+//     </li>
+//     <li>
+//       <div class="category-navigation-heading">快速入门</div>
+//       <ul class="category-navigation-section">
+//         <li><a href="#">概述</a></li>
+//         <li><a href="#">云端管理</a></li>
+//       </ul>
+//     </li>
+//   </ul>
+// </div>
